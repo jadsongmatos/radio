a955ce0049483dbdfec61f09094b2e8533ffcd16
jadsongmatos <jadson.g-matos@outlook.com>
2025-12-18 14:49:54 -0300

    feat: atualizar título da página e adicionar verificação do Google Site

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -24,7 +24,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'Web Radio J',
       },
     ],
     links: [
@@ -44,6 +44,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       <head>
         <HeadContent />
         <meta name="google-adsense-account" content="ca-pub-7540935582112706"></meta>
+        <meta name="google-site-verification" content="K6n_i0D944OJIJwD-M5iQ-jy3oAKFS5aTTL3uJOpy9I" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7540935582112706"
           crossOrigin="anonymous"></script>
       </head>
